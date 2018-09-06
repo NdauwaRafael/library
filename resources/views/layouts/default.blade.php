9e0560e6d70768ac3925e4431e94ba9d979d08d0
Raphael Karanja <rkaranja@cytonn.com>
2018-09-06 22:19:05 +0300

    setup main files and setup main pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+@include('layouts.partials.topbar')
+
+{{--@yield('content)--}}
+
+@include('layouts.partials.footer')
+
+
+<script src="js/app.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
---
+++ resources/views/layouts/partials/topbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="topbar">
+    <div class="topbar__title">
+        Modern Library
+    </div>
+
+    <div class="topbar__content">
+        <div class="menus">
+                <span><a href="/login">Sign In</a></span>
+                <span><a href="/register">Sign Up</a></span>
+        </div>
+    </div>
+
+</nav>
