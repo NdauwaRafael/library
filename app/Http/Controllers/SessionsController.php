934acbe20e5206e0b571d0da34e11ada9cfb3a71
Raphael Karanja <rkaranja@cytonn.com>
2018-09-07 07:18:31 +0300

    rename app name

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SessionController extends Controller
+class SessionsController extends Controller
 {
     public function __contruct(){
         $this->middleware('guest', ['except'=> 'destroy']);
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
     <div class="dashboard">
-        
+
     </div>
 @endsection
