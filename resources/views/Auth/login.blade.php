b940707899b2a12aaf59adc25213c3f8159de11c
Raphael Karanja <rkaranja@cytonn.com>
2018-09-07 09:03:17 +0300

    add submit login function

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,16 +3,23 @@
     <div class="container">
         <div class="login">
             <div class="login__card">
-                <form action="">
+                <form action="/login" method="post">
+                    {{csrf_field()}}
                     <label>User Email
-                        <input type="email" placeholder="enter user email">
+                        <input  type="email" name="email" placeholder="enter user email" value="{{old('email')}}" required>
                     </label>
 
                     <label>User Password
-                        <input type="password" placeholder="enter user password">
+                        <input type="password" name="password" placeholder="enter user password" required>
                     </label>
 
-                    <button class="success button">Sign In</button>
+                    <button type="post" class="success button">Sign In</button>
+
+                    <span class="login__error">
+                        @foreach($errors->all() as $error)
+                            {{$error}}
+                        @endforeach
+                    </span>
                 </form>
             </div>
         </div>
