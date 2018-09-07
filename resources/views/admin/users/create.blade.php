001fb5b2ea1dd414b63f136bac95f4c545f3dad9
Raphael Karanja <rkaranja@cytonn.com>
2018-09-07 17:31:24 +0300

    finalise styling user form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,20 +8,20 @@
      <create-user :roles="{{ $roles }}" :departments="{{ $departments }}" inline-template>
          <div>
              <label>Username
-                 <el-input placeholder="username"  name="username"></el-input>
+                 <el-input placeholder="username"  name="username" value="{{old('username')}}" required></el-input>
              </label>
 
              <label>First Name
-                 <el-input placeholder="First Name"  name="firstname"></el-input>
+                 <el-input placeholder="First Name"  name="firstname" value="{{old('firstname')}}" required></el-input>
              </label>
              <label>Last Name
-                 <el-input placeholder="Last Name"  name="lastname"></el-input>
+                 <el-input placeholder="Last Name"  name="lastname" value="{{old('lastname')}}" required></el-input>
              </label>
              <label>Email
-                 <el-input  type="email" placeholder="Email"  name="email"  ></el-input>
+                 <el-input  type="email" placeholder="Email"  name="email" value="{{old('email')}}" required></el-input>
              </label>
              <label>Department
-                 <select name="department_id" >
+                 <select name="department_id" required>
                      <option value="" label="Select Department" disabled selected></option>
                      <option v-for="department in departments"
                              :label="department.label"
@@ -29,7 +29,7 @@
                  </select>
              </label>
              <label>Role
-                 <select name="role_id" >
+                 <select name="role_id" required>
                      <option value="" label="Select A Role" disabled selected></option>
                      <option v-for="role in roles"
                              :label="role.label"
