3bcd38a1f33da92ebe6a9e7adc1ff9d0a26c661f
Raphael Karanja <rkaranja@cytonn.com>
2018-09-10 19:06:10 +0300

    style view permissions page

--- resources/views/admin/roles/show.blade.php
+++ resources/views/admin/roles/show.blade.php
@@ -1,43 +1,41 @@
 @extends('layouts.default')
 @section('content')
     <section>
-        <div>
-            <div>
-                <div>
+        <h3>Role Permission</h3>
+
+        <div class="lib_page">
+            <div class="lib_table">
+                <div class="lib_table__head">
+                    <div class="action">
+                        <a href="/roles/permissions/{!! $role->id !!}" class="masaba_primary">
+                            Edit Permission
+                        </a>
+                    </div>
+                    <div class="filters"></div>
                     <div>
                         <div >
-                            <h3>Role Permission</h3>
-                        </div>
-                        <div >
                             <div>
-                                <a href="/roles/permissions/{!! $role->id !!}" class="masaba_primary">
-                                    Edit Permission
-                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div >
-                    <div >
-                        <div>
-                            <table>
-                                <thead>
-                                <tr>
-                                    <td>Name</td>
-                                    <td>Description</td>
-                                </tr>
-                                </thead>
+                <div class="lib_table__body">
+                    <table>
+                        <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Description</td>
+                        </tr>
+                        </thead>
 
-                                @foreach($role->rolePermissions as $permission)
-                                    <tr>
-                                        <td>{!! $permission->permission->name!!}</td>
-                                        <td>{!! $permission->permission->description !!}</td>
-                                    </tr>
-                                @endforeach
+                        @foreach($role->rolePermissions as $permission)
+                            <tr>
+                                <td>{!! $permission->permission->name!!}</td>
+                                <td>{!! $permission->permission->description !!}</td>
+                            </tr>
+                        @endforeach
 
-                            </table>
-                        </div>
-                    </div>
+                    </table>
                 </div>
             </div>
         </div>
