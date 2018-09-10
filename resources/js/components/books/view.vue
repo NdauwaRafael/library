03bf0ab93eab84c33e096e32390e8a31da1f73d7
Raphael Karanja <rkaranja@cytonn.com>
2018-09-10 16:31:17 +0300

    update reserve book form

--- resources/js/components/books/view.vue
+++ resources/js/components/books/view.vue
@@ -59,7 +59,7 @@
                         <td>{{book.created_at}}</td>
                         <td>
                             <el-button type="primary" @click="editBook = true" icon="el-icon-edit" circle></el-button>
-                            <el-button icon="el-icon-message" @click="deleteBook = true" >Request</el-button>
+                            <el-button icon="el-icon-message" @click="reserveBook = true" >Request</el-button>
                             <el-button type="danger" icon="el-icon-delete" @click="deleteBook = true" circle></el-button>
                         </td>
                     </tr>
